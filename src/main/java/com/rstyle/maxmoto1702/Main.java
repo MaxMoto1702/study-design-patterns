007eb9dbe7103b985ddbb441a35e409b4a3a0e78
Max Serebryanskiy <Maksim.Serebryanskiy@R-Style.com>
2015-04-12 18:47:20 +0300

    Fix bugs in main class

--- src/main/java/com/rstyle/maxmoto1702/Main.java
+++ src/main/java/com/rstyle/maxmoto1702/Main.java
@@ -1,30 +1,14 @@
 package com.rstyle.maxmoto1702;
 
 import com.rstyle.maxmoto1702.desingpatterns.AbstractFactoryPatternDemo;
-import com.rstyle.maxmoto1702.desingpatterns.AdapterPatternDemo;
 import com.rstyle.maxmoto1702.desingpatterns.BridgePatternDemo;
 import com.rstyle.maxmoto1702.desingpatterns.BuilderPatternDemo;
-import com.rstyle.maxmoto1702.desingpatterns.AbstractFactoryDemo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static Logger LOG = LoggerFactory.getLogger(Main.class);
-
     public static void main(String... args) {
         AbstractFactoryPatternDemo.main(args);
-        AdapterPatternDemo.main(args);
+        AbstractFactoryPatternDemo.main(args);
         BridgePatternDemo.main(args);
-    }
-
-    public static void main(String... args) {
         BuilderPatternDemo.main(args);
-        AbstractFactoryDemo.main(args);
-    }
-
-    public int method() {
-        int var = 0;
-        LOG.debug("" + var);
-        return var;
     }
 }
